9236024db8c33c26cb7e230f2042bd73b8f50dc1
AaronSunil <72694245+AaronSunil@users.noreply.github.com>
2023-05-05 20:39:20 +0530

    Add files via upload

--- flask_app.py
+++ flask_app.py
@@ -1,33 +1,22 @@
 from flask import Flask, jsonify, request
 app = Flask(__name__)
 
-tasks = [
+List = [
     {
         'id': 1,
-        'title': u'Buy Groceries',
-        'descrption': u'Milk, Cheese, Pizza, Fruits',
+        'Contact': u'85564595600',
+        'Name': u'Raju',
         'done': False
     },
 
     {
         'id': 2,
-        'title': u'Learn Python',
-        'descrption': u'Need to find a good python tutorial on web',
+        'Contact': u'7826264554',
+        'Name': u'Rahul',
         'done': False
     },
 ]
  
-contact = {
-
-    'id': tasks[-1]['id'] + 1,
-
-    'Name': request.json['Name'],
-
-    'Contact': request.json.get('Contact', ""),
-
-    'done': False
-}
-
 @app.route("/")
 
 def hello_world():
@@ -41,9 +30,27 @@ def add_task():
             "status":"error",
             "message": "Please provide the data!" 
             },400) 
+    
+    contact = {
+
+    'id': List[-1]['id'] + 1,
+
+    'Name': request.json['Name'],
+
+    'Contact': request.json.get('Contact', ""),
+
+    'done': False
+    }
+    List.append(contact)
+    return jsonify({
+        "status":"success",
+        "message":"Contact Added Successfully"
+    })
+
+
 @app.route("/get-data") 
 
 def add_task(): 
-    return jsonify({ "data" : tasks }) 
+    return jsonify({ "data" : List }) 
     if (__name__ == "__main__"): 
         app.run(debug=True)
